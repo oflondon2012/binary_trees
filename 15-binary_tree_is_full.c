bd97b462808dfe2483d1198a3cfe24ab50b2e39f
oflondon2012 <gtadebayo2023@gmail.com>
2024-03-01 22:28:46 +0000

    binary tree update

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,19 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full - function to check if binary tree is full
- * @tree: pointer to the root of the node
- *
- * Return: tranverse node of a tree
- */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (traverse_node(tree));
-}
-
-/**
  * traverse_node - function the check the traverse of
  * binary tree
  * @tree: pointer to the root of the tree
@@ -27,4 +14,16 @@ int traverse_node(const binary_tree_t *tree)
 	if (tree->right != NULL && tree->left != NULL)
 		return (traverse_node(tree->right) && traverse_node(tree->left));
 	return (0);
+}
+/**
+ * binary_tree_is_full - function to check if binary is full
+ * @tree: pointer to the root of the node
+ *
+ * Return: Traverse node of a tree
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (traverse_node(tree));
 }
